6143ab10630c7acf50af23985caa6c2b5b6326fc
ShunkiSaito <s.saito@4nonome.com>
2022-10-02 13:36:01 +0900

    organisms.header.stories.js

---
+++ src/stories/organisms-header.stories.js
@@ -0,0 +1,6 @@
+import organisms_header from "../components/organisms/header/header.html";
+import "../components/organisms/header/header.scss";
+
+export default { title: "organisms/header" };
+
+export const header = () => organisms_header;
