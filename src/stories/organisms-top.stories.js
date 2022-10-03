847c7ec70b3ae255946bf3d6927626212f2e336e
ShunkiSaito <s.saito@4nonome.com>
2022-10-03 14:18:19 +0900

    organisms-top.stories.js

---
+++ src/stories/organisms-top.stories.js
@@ -0,0 +1,6 @@
+import organisms_top from "../components/organisms/top/top.html";
+import "../components/organisms/top/top.scss";
+
+export default { title: "Organisms/top" };
+
+export const top = () => organisms_top;
