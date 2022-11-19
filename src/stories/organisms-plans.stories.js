8a2463b06ab5792ebd0da2cf3b3e616a03e7ecdc
ShunkiSaito <s.saito@4nonome.com>
2022-11-19 11:06:04 +0900

    organism-plans.stories.js

---
+++ src/stories/organisms-plans.stories.js
@@ -0,0 +1,6 @@
+import organisms_plans from "../components/organisms/plans/plans.html";
+import "../components/organisms/plans/plans.scss";
+
+export default { title: "Organisms/plans" };
+
+export const plans = () => organisms_plans;
