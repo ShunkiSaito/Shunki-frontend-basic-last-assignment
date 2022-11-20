30b347b1cb0273238d628172e74346226ec762cf
ShunkiSaito <s.saito@4nonome.com>
2022-11-20 12:10:43 +0900

    footer.stories.js

---
+++ src/stories/organisms-footer.stories.js
@@ -0,0 +1,6 @@
+import organisms_footer from "../components/organisms/footer/footer.html";
+import "../components/organisms/footer/footer.scss";
+
+export default { title: "organisms/footer" };
+
+export const footer = () => organisms_footer;
